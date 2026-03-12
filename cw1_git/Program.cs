5da8bf33db2849c7b33b20ce106e75536efa62c2
Yauheniya Nikalyenka <s32570@pjwstk.edu.pl>
2026-03-12 20:09:51 +0100

    [1]dev branch --> Added: sum of two doubles method.

--- cw1_git/Program.cs
+++ cw1_git/Program.cs
@@ -1,3 +1,15 @@
 ﻿
 Console.WriteLine("Initial git setup."); //first commit [0]
 
+//Second commit --> dev [1]
+Console.WriteLine("Sum() method added.");
+static double Sum(double a, double b)
+{
+    Console.WriteLine("Sum of " + a + " and " + b);
+    Console.WriteLine("Result:");
+    return a + b;
+}
+
+Console.WriteLine("Test:\n");
+double result = Sum(5.5, 2.75);
+Console.WriteLine(result);
